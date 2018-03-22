62a54c42eb0d45d8ce79c7c780674a41c8ff9b4d
Chris <ccamer3940@greenvilleschools.us>
2018-03-22 10:24:22 -0400

    Added Item

---
+++ 2006/Item/Vehicle.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Vehicle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Vehicle
+{
+    private double dealerCost;
+    private double dealerMarkup;
+    
+    
+}
