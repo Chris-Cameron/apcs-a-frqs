43fbdae4d2c0d11d2211b97a524dc1763fcd8805
Chris <ccamer3940@greenvilleschools.us>
2018-03-23 15:04:19 -0400

    Item FRQ

--- 2006/Item/ItemTester.java
+++ 2006/Item/ItemTester.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class ItemTester here.
+ * Tests the interface item and its subclasses
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @Christopher Cameron 
+ * @v1
  */
 public class ItemTester
 {
@@ -11,22 +11,24 @@ public class ItemTester
     {
         //Set variables for printing
         NonTaxableItem NT1 = new NonTaxableItem(22222);
-        Vehicle V1 = new Vehicle(20000,2500,0.10);
-        Vehicle V2 = new Vehicle(20000,1000,0.10);
+        Vehicle V = new Vehicle(20000,2500,0.10);
         
         //Print Stuff
         System.out.println("Non-Taxable Item Price:");
         System.out.println(NT1.purchasePrice());
-        System.out.println("\nVehicle 1 Prices:");
+        
+        System.out.println("\nVehicle Prices:");
         System.out.print("List Price: ");
-        System.out.println(V1.getListPrice());
+        System.out.println(V.getListPrice());
         System.out.print("Purchase Price: ");
-        System.out.println(V1.purchasePrice());
-        System.out.println("\nVehicle 2 Prices:");
+        System.out.println(V.purchasePrice());
+        
+        V.changeMarkup(1000);
+        
+        System.out.println("\nVehicle Price After Markup Change:");
         System.out.print("List Price: ");
-        System.out.println(V2.getListPrice());
+        System.out.println(V.getListPrice());
         System.out.print("Purchase Price: ");
-        System.out.println(V2.purchasePrice());
-        /** ADD TEST CASE FOR CHANGING RATE */
+        System.out.println(V.purchasePrice());
     }
 }
